c6e1bfd718035e6b5926faf5a55de511b07b80fd
D <deilee0913@gmail.com>
2025-11-24 14:25:25 +0900

    refactor: Race 계약의 우승자 문자열 생성 로직을 StringUtils 라이브러리로 분리

--- solidity-racingcar/contracts/services/Race.sol
+++ solidity-racingcar/contracts/services/Race.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "../interfaces/IRace.sol";
 import "../libraries/CarStruct.sol";
 import "../libraries/RandomMoveLibrary.sol";
+import "../libraries/StringUtils.sol";
 
 contract Race is IRace {
     using CarStruct for CarStruct.Car;
@@ -21,7 +22,7 @@ contract Race is IRace {
         CarStruct.Car[] storage _cars
     ) internal view override returns (string memory) {
         uint256 maxPos = _findMaxPosition(_cars);
-        return _buildWinnerText(_cars, maxPos);
+        return StringUtils.buildWinnerText(_cars, maxPos);
     }
 
     function _findMaxPosition(
@@ -43,42 +44,4 @@ contract Race is IRace {
         }
         return _currentMax;
     }
-
-    function _buildWinnerText(
-        CarStruct.Car[] storage _cars,
-        uint256 _maxPos
-    ) private view returns (string memory) {
-        string memory result = "";
-        bool isFirst = true;
-
-        for (uint256 i = 0; i < _cars.length; i++) {
-            if (_cars[i].position == _maxPos) {
-                result = _appendName(result, _cars[i].name, isFirst);
-                isFirst = false;
-            }
-        }
-        return result;
-    }
-
-    function _appendName(
-        string memory _current,
-        string memory _name,
-        bool _isFirst
-    ) private pure returns (string memory) {
-        if (!_isFirst) {
-            return string.concat(_current, ", ", _name);
-        }
-        return string.concat(_current, _name);
-    }
-
-    // // 두 바이트 배열을 연결하는 헬퍼 함수
-    // // 문자열 연결을 위해 어셈블리 사용 (가스 효율성 목적)
-    // function _concatenateBytes(bytes memory a, bytes memory b) private pure returns (bytes memory) {
-    //     bytes memory c = new bytes(a.length + b.length);
-    //     assembly {
-    //         mstore(add(c, 32), a)
-    //         mstore(add(add(c, 32), mload(a)), b)
-    //     }
-    //     return c;
-    //
 }
